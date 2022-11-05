b3a95089264560e1c13487d97e6035cc1758ab48
Pedro Antoninho <pdn.antoninho@gmail.com>
2022-11-05 00:37:05 +0000

    add betToProposal on DABets

--- src/DABets.sol
+++ src/DABets.sol
@@ -26,6 +26,8 @@ contract DABets {
 
     DABookie bookie;
     mapping(uint256 => Proposal) proposals;
+    // betId => proposalId
+    mapping(uint256 => uint256) betToProposal;
     // betId => staked amount
     mapping(uint256 => uint256) betStakes;
     // address => betId => stake
@@ -79,8 +81,10 @@ contract DABets {
         betId = _placedBetId(proposalId, bet);
 
         // bet does not exist yet, add it to proposal bets array
+        // and link their ids in betToProposal
         if (betStakes[betId] == 0) {
             proposals[proposalId].bets.push(bet);
+            betToProposal[betId] = proposalId;
         }
 
         // increment player staked amount on this bet
@@ -137,7 +141,8 @@ contract DABets {
     }
 
     /**
-     * @notice  Calculates a player's rewards on a placed bet
+     * @notice  Calculates a player's current potential rewards on a placed bet
+     * @dev     This may be called while the bet is still ongoing. It calculates the rewards if the player's bet was a winner.
      * @param   player  the player address
      * @param   betId  the bet id
      * @return  rewards  the reward
@@ -145,14 +150,10 @@ contract DABets {
     function calculateRewards(address player, uint256 betId)
         public
         view
+        ensureBetExists(betId)
         returns (uint256 rewards)
     {
-        uint256 proposalId = validBets[betId];
-
-        if (proposalId == 0) {
-            return 0;
-        }
-
+        uint256 proposalId = betToProposal[betId];
         DABets.Proposal memory proposal = proposals[proposalId];
 
         uint256 totalStake = proposal.betPool;
@@ -188,6 +189,13 @@ contract DABets {
     modifier ensureProposalExists(uint256 id) {
         if (proposals[id].creator == address(0)) {
             revert ProposalNotFound();
+        }
+        _;
+    }
+
+    modifier ensureBetExists(uint256 id) {
+        if (betStakes[id] == 0) {
+            revert BetNotFound();
         }
         _;
     }
--- src/Errors.sol
+++ src/Errors.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 error Unauthorized();
 error ProposalNotFound();
+error BetNotFound();
 error DuplicateBet();
 error ClosedBets();
 error InsufficientStake();
